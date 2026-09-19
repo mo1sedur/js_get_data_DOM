8114f4c8d1d40322030e7474f76c25f540b17a87
Denys <denny@Denyss-MacBook-Pro.local>
2026-09-19 13:58:55 +0200

    finished this exercise

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,13 @@
 'use strict';
 
-// write your code here
+const populations = document.getElementsByClassName('population');
+const result = Array.from(populations).map( item => {
+   return Number(item.textContent.replaceAll(',', ''));
+});
+
+const total = result.reduce((acc, currentItem) => acc + currentItem, 0);
+const average = total / result.length;
+
+
+document.querySelector('.total-population').textContent = total.toLocaleString('en-US');
+document.querySelector('.average-population').textContent = average.toLocaleString('en-US', { maximumFractionDigits: 2 });
